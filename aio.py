7f5b21fdbcb58bee168d1d10e9c9a992a3186d6b
Sean Patten <seanpattencode@users.noreply.github.com>
2026-01-02 02:43:15 -0500

    aio set alias

--- aio.py
+++ aio.py
@@ -1031,7 +1031,7 @@ COMMANDS = {
     'update': cmd_update, 'jobs': cmd_jobs, 'kill': cmd_kill, 'killall': cmd_kill, 'attach': cmd_attach,
     'cleanup': cmd_cleanup, 'config': cmd_config, 'ls': cmd_ls, 'diff': cmd_diff, 'send': cmd_send,
     'watch': cmd_watch, 'push': cmd_push, 'pull': cmd_pull, 'revert': cmd_revert, 'setup': cmd_setup,
-    'install': cmd_install, 'deps': cmd_deps, 'prompt': cmd_prompt, 'gdrive': cmd_gdrive, 'note': cmd_note, 'settings': cmd_settings,
+    'install': cmd_install, 'deps': cmd_deps, 'prompt': cmd_prompt, 'gdrive': cmd_gdrive, 'note': cmd_note, 'set': cmd_settings, 'settings': cmd_settings,
     'add': cmd_add, 'remove': cmd_remove, 'rm': cmd_remove, 'dash': cmd_dash, 'a': cmd_multi, 'all': cmd_multi,
     'e': cmd_e, 'x': cmd_x, 'p': cmd_p, 'copy': cmd_copy, 'dir': lambda: (print(f"📂 {os.getcwd()}"), sp.run(['ls'])),
     'fix': cmd_fix_bug_feat_auto_del, 'bug': cmd_fix_bug_feat_auto_del, 'feat': cmd_fix_bug_feat_auto_del,
